76dc22a466111ab87a7983f9e28d833607e0c0d4
cclngit <38783129+cclngit@users.noreply.github.com>
2023-03-20 19:53:46 +0100

    initial source code

---
+++ Fireware/MTR_Tiny_MD/src/main.cpp
@@ -0,0 +1,153 @@
+#include <Arduino.h>
+#include <SimpleFOC.h>
+#include <SimpleFOCDrivers.h>
+#include "drivers/drv8316/drv8316.h"
+
+#define TARGET_RP2040
+
+// Prototype for the function
+void printDRV8316Status();
+
+// magnetic sensor instance - MagneticSensorI2C
+MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
+
+
+// BLDC motor & driver instance
+BLDCMotor motor = BLDCMotor(11);
+DRV8316Driver6PWM driver = DRV8316Driver6PWM(0,1,2,3,4,6,7,false); // use the right pins for your setup!
+
+// velocity set point variable
+float target_velocity = 0;
+// instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
+
+void setup() {
+
+  // initialise magnetic sensor hardware
+  sensor.init();
+  // link the motor to the sensor
+  motor.linkSensor(&sensor);
+
+  // driver config
+  // power supply voltage [V]
+  driver.voltage_power_supply = 12;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver);
+
+  // set motion control loop to be used
+  motor.controller = MotionControlType::velocity;
+
+  // contoller configuration
+  // default parameters in defaults.h
+
+  // velocity PI controller parameters
+  motor.PID_velocity.P = 0.2f;
+  motor.PID_velocity.I = 20;
+  motor.PID_velocity.D = 0;
+  // default voltage_power_supply
+  motor.voltage_limit = 6;
+  // jerk control using voltage voltage ramp
+  // default value is 300 volts per sec  ~ 0.3V per millisecond
+  motor.PID_velocity.output_ramp = 1000;
+
+  // velocity low pass filtering
+  // default 5ms - try different values to see what is the best.
+  // the lower the less filtered
+  motor.LPF_velocity.Tf = 0.01f;
+
+  // use monitoring with serial
+  Serial.begin(115200);
+  // comment out if not needed
+  motor.useMonitoring(Serial);
+
+  // initialize motor
+  motor.init();
+  // align sensor and start FOC
+  motor.initFOC();
+
+  // add target command T
+  command.add('T', doTarget, "target velocity");
+
+  Serial.println(F("Motor ready."));
+  Serial.println(F("Set the target velocity using serial terminal:"));
+  _delay(1000);
+}
+
+void loop() {
+
+  // Sensor update
+  sensor.update();
+
+  // main FOC algorithm function
+  motor.loopFOC();
+
+  // Motion control function
+  motor.move(target_velocity);
+
+  // function intended to be used with serial plotter to monitor motor variables
+  motor.monitor();
+
+  // user communication
+  command.run();
+}
+
+void printDRV8316Status() {
+	DRV8316Status status = driver.getStatus();
+	Serial.println("DRV8316 Status:");
+	Serial.print("Fault: ");
+	Serial.println(status.isFault());
+	Serial.print("Buck Error: ");
+	Serial.print(status.isBuckError());
+	Serial.print("  Undervoltage: ");
+	Serial.print(status.isBuckUnderVoltage());
+	Serial.print("  OverCurrent: ");
+	Serial.println(status.isBuckOverCurrent());
+	Serial.print("Charge Pump UnderVoltage: ");
+	Serial.println(status.isChargePumpUnderVoltage());
+	Serial.print("OTP Error: ");
+	Serial.println(status.isOneTimeProgrammingError());
+	Serial.print("OverCurrent: ");
+	Serial.print(status.isOverCurrent());
+	Serial.print("  Ah: ");
+	Serial.print(status.isOverCurrent_Ah());
+	Serial.print("  Al: ");
+	Serial.print(status.isOverCurrent_Al());
+	Serial.print("  Bh: ");
+	Serial.print(status.isOverCurrent_Bh());
+	Serial.print("  Bl: ");
+	Serial.print(status.isOverCurrent_Bl());
+	Serial.print("  Ch: ");
+	Serial.print(status.isOverCurrent_Ch());
+	Serial.print("  Cl: ");
+	Serial.println(status.isOverCurrent_Cl());
+	Serial.print("OverTemperature: ");
+	Serial.print(status.isOverTemperature());
+	Serial.print("  Shutdown: ");
+	Serial.print(status.isOverTemperatureShutdown());
+	Serial.print("  Warning: ");
+	Serial.println(status.isOverTemperatureWarning());
+	Serial.print("OverVoltage: ");
+	Serial.println(status.isOverVoltage());
+	Serial.print("PowerOnReset: ");
+	Serial.println(status.isPowerOnReset());
+	Serial.print("SPI Error: ");
+	Serial.print(status.isSPIError());
+	Serial.print("  Address: ");
+	Serial.print(status.isSPIAddressError());
+	Serial.print("  Clock: ");
+	Serial.print(status.isSPIClockFramingError());
+	Serial.print("  Parity: ");
+	Serial.println(status.isSPIParityError());
+	if (status.isFault())
+		driver.clearFault();
+	delayMicroseconds(1); // ensure 400ns delay
+	DRV8316_PWMMode val = driver.getPWMMode();
+	Serial.print("PWM Mode: ");
+	Serial.println(val);
+	delayMicroseconds(1); // ensure 400ns delay
+	bool lock = driver.isRegistersLocked();
+	Serial.print("Lock: ");
+	Serial.println(lock);
+}
