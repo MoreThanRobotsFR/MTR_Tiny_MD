a19dc32c40bd53ba0a301519f98fe0e2636959e3
cclngit <38783129+cclngit@users.noreply.github.com>
2023-08-09 04:10:41 +0200

    add local libraries and NeoPixel Hello World

---
+++ Fireware/MTR_Tiny_MD/include/Tiny_MD_Params.h
@@ -0,0 +1,27 @@
+#define WS2812B_PIN         25
+#define NUMPIXELS           1 
+
+#define DRV8316_INL_A       14
+#define DRV8316_INH_A       15
+#define DRV8316_INL_B       12
+#define DRV8316_INH_B       13
+#define DRV8316_INL_C       10
+#define DRV8316_INH_C       11
+#define DRV8316_nFAULT      7
+#define DRV8316_nSLEEP      6
+#define DRV8316_DRV_OFF     8
+#define DRV8316_SPI_CS      17
+
+
+#define DRV8316_CSENSE_A    26
+#define DRV8316_CSENSE_B    27
+#define DRV8316_CSENSE_C    28
+#define DRV8316_VREF_ADC    29
+
+int Red = 0;
+int Green = 0;
+int Blue = 0;
+
+
+
+
